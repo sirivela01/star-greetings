907083d3b931defeaa73ea716f0489eccde5e3d8
Star Greetings Author <author@stargreetings.com>
2026-07-22 20:40:01 +0530

    feat: replace Keerthy Suresh greeting card with Dasara image and rename

--- js/config.js
+++ js/config.js
@@ -23,7 +23,7 @@ const STAR_CONFIG = {
     { id: "pooja_hegde", name: "Ala Vaikuntapurramulo", industry: "Tollywood", imagePath: "assets/stars/pooja_hegde.png", movie: "Ala Vaikunthapurramuloo" },
     { id: "nani", name: "Nani", industry: "Tollywood", imagePath: "assets/stars/nani.png", movie: "HIT: The Third Case" },
     { id: "vijay_deverakonda", name: "Vijay Deverakonda", industry: "Tollywood", imagePath: "assets/stars/vijay_deverakonda.png", movie: "Arjun Reddy" },
-    { id: "keerthy_suresh", name: "Keerthy Suresh", industry: "Tollywood", imagePath: "assets/stars/keerthy_suresh.png", movie: "Dasara" },
+    { id: "keerthy_suresh", name: "Dasara", industry: "Tollywood", imagePath: "assets/stars/keerthy_suresh.png", movie: "Dasara" },
     { id: "anushka_shetty", name: "Anushka", industry: "Tollywood", imagePath: "assets/stars/anushka_shetty.png", movie: "Mirchi" },
     { id: "kajal_aggarwal", name: "Kajal Agharwal", industry: "Tollywood", imagePath: "assets/stars/kajal_aggarwal.png", movie: "Kajal Agharwal" },
     { id: "sai_pallavi", name: "Maari 2", industry: "Tollywood", imagePath: "assets/stars/sai_pallavi.png", movie: "Maari 2" },
